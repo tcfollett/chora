ed40b653c25d91c9afbd0f681244dd641b1116b7
tcfollett <follettsfive.toby@icloud.com>
2026-06-24 14:15:10 -0700

    Made changes to size()
    size() now uses .iter().product and returns size. Tests implemented for size(). All passed

--- src/shape.rs
+++ src/shape.rs
@@ -5,10 +5,29 @@ pub struct TensorShape {
 
 impl TensorShape {
     pub fn size(&self) -> usize {
-        let mut size = 1;
-        for i in &self.shape {
-            size *= i;
-        }
-        return size;
+        self.shape.iter().product()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn size_of_2x3() {
+        let shape = TensorShape { shape: vec![2, 3] };
+        assert_eq!(shape.size(), 6);
+    }
+
+    #[test]
+    fn size_of_1() {
+        let shape = TensorShape { shape: vec![4] };
+        assert_eq!(shape.size(), 4);
+    }
+
+    #[test]
+    fn size_of_0() {
+        let shape = TensorShape { shape: vec![] };
+        assert_eq!(shape.size(), 1)
     }
 }
