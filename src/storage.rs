b46fd12a54fab3961b6b2b455d3ec07f46f45025
tcfollett <follettsfive.toby@icloud.com>
2026-06-25 12:44:16 -0700

    Finished TensorStorage and zeros()
    All tests passed. zero() takes a usize rather than &TensorShape since storage only needs the total element count. Keeps storage resusable for operations like reshape

--- src/storage.rs
+++ src/storage.rs
@@ -3,6 +3,21 @@ pub struct TensorStorage<T> {
     data: Vec<T>,
 }
 
-impl<T> TensorStorage<T> {
-    pub fn zeros() {}
+impl<T: Default + Clone> TensorStorage<T> {
+    pub fn zeros(size: usize) -> Self {
+        let zeros = vec![T::default(); size];
+        Self { data: zeros }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero() {
+        let storage: TensorStorage<i32> = TensorStorage::zeros(3);
+        assert_eq!(storage.data.len(), 3);
+        assert_eq!(storage.data, vec![0, 0, 0]);
+    }
 }
