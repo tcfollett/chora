35fec33f09e9e6262923e5b0a29cb40b506e35b7
tcfollett <follettsfive.toby@icloud.com>
2026-06-26 15:31:28 -0700

    Test passed

--- tests/tensor_tests.rs
+++ tests/tensor_tests.rs
@@ -1,9 +1,9 @@
 use chora::{Tensor, TensorShape};
 
 #[test]
-fn test_name_here() {
-    // 1. build a TensorShape, e.g. shape [2, 3]
-    // 2. build a Tensor with mut binding and some known data, e.g. 0..6
-    // 3. mutate one element via index_mut: tensor[&[r, c]] = some_value;
-    // 4. read it back: assert_eq!(tensor[&[r, c]], some_value);
+fn test1() {
+    let shape = TensorShape::new(vec![2, 3]);
+    let mut tensor = Tensor::new(shape, vec![0, 1, 2, 3, 4, 5]);
+    tensor[&[1, 2]] = 8;
+    assert_eq!(tensor[&[1, 2]], 8);
 }
