b877e2b631df0b83355f2bb593987db16a4906ef
tcfollett <follettsfive.toby@icloud.com>
2026-06-26 15:24:17 -0700

    Move tests/ to correct place

---
+++ tests/tensor_tests.rs
@@ -0,0 +1,9 @@
+use chora::{Tensor, TensorShape};
+
+#[test]
+fn test_name_here() {
+    // 1. build a TensorShape, e.g. shape [2, 3]
+    // 2. build a Tensor with mut binding and some known data, e.g. 0..6
+    // 3. mutate one element via index_mut: tensor[&[r, c]] = some_value;
+    // 4. read it back: assert_eq!(tensor[&[r, c]], some_value);
+}
