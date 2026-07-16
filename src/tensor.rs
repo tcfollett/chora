57f476c110fe95ea830a88ead48731f0ee97dfe1
tcfollett <follettsfive.toby@icloud.com>
2026-07-15 23:33:47 -0700

    created Tensor struct calculates strides, shape method

--- src/tensor.rs
+++ src/tensor.rs
@@ -3,3 +3,28 @@
 // tensor struct will hold shape, strides, and storage (from backend)
 // also have contructors like zeros(), ones(), new()
 // trait impls like Clone, Debug, PartialEq, and Display
+use crate::backend::Backend;
+
+pub struct Tensor<B: Backend> {
+    shape: Vec<usize>,
+    strides: Vec<usize>,
+    data: B::Storage,
+    backend: B,
+}
+
+fn strides(shape: &[usize]) -> Vec<usize> {
+    let mut stride_list = Vec::with_capacity(shape.len());
+    let mut current_stride = 1;
+    for i in shape.iter().rev() {
+        stride_list.push(current_stride);
+        current_stride *= i;
+    }
+    stride_list.reverse();
+    stride_list
+}
+
+impl<B: Backend> Tensor<B> {
+    fn shape(&self) -> &[usize] {
+        &self.shape
+    }
+}
