c86028bc70bf7e206fceaab924520ae91644c7df
tcfollett <follettsfive.toby@icloud.com>
2026-07-16 18:28:47 -0700

    update errors

--- src/error.rs
+++ src/error.rs
@@ -2,5 +2,16 @@
 
 #[derive(Debug)]
 pub enum TensorError {
-    ShapeMismatch,
+    ShapeMismatch {
+        shape1: Vec<usize>,
+        shape2: Vec<usize>,
+    }, // two tensors with incompatible shape
+    OutOfBounds {
+        index: Vec<usize>,
+        shape: Vec<usize>,
+    }, // index does not match dimensions of the tensor
+    DataShapeMismatch {
+        data: usize,
+        shape: Vec<usize>,
+    }, // data does not fit the shape of the tensor
 }
